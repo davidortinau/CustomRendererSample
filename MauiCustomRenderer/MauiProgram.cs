8f265bd928c383ed41ddf660eea93cc4f41a5c63
David Ortinau <david.ortinau@microsoft.com>
2022-04-30 12:55:59 -0500

    added use maui compat

--- MauiCustomRenderer/MauiProgram.cs
+++ MauiCustomRenderer/MauiProgram.cs
@@ -10,6 +10,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCompatibility()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
