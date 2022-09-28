d642c4e66f163e7f92ddd523707c19b749e46251
David Ortinau <david.ortinau@microsoft.com>
2022-09-28 09:15:35 -0500

    removed Frame

--- MauiCustomRenderer/MauiProgram.cs
+++ MauiCustomRenderer/MauiProgram.cs
@@ -5,28 +5,27 @@ namespace MauiCustomRenderer;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMauiCompatibility()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			})
-            .ConfigureMauiHandlers((handlers) =>{
+public static MauiApp CreateMauiApp()
+{
+	var builder = MauiApp.CreateBuilder();
+	builder
+		.UseMauiApp<App>()
+		.UseMauiCompatibility()
+		.ConfigureFonts(fonts =>
+		{
+			fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+		})
+        .ConfigureMauiHandlers((handlers) =>{
 #if ANDROID
-				handlers.AddCompatibilityRenderer(typeof(PressableView),typeof(XamarinCustomRenderer.Droid.Renderers.PressableViewRenderer));
+			handlers.AddHandler(typeof(PressableView),typeof(XamarinCustomRenderer.Droid.Renderers.PressableViewRenderer));
 #endif
 
 #if IOS
-                handlers.AddCompatibilityRenderer(typeof(PressableView), typeof(XamarinCustomRenderer.iOS.Renderers.PressableViewRenderer));
+            handlers.AddHandler(typeof(PressableView), typeof(XamarinCustomRenderer.iOS.Renderers.PressableViewRenderer));
 #endif
-            })
-			;
+        });
 
-		return builder.Build();
-	}
+	return builder.Build();
+}
 }
